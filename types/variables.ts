5c2c0338f14ac5e0918c27a753f4f8cfca86739b
Chinmay Mhatre <chinmaymhatre12@gmail.com>
2023-02-10 17:39:00 +0530

    basic string variable types

---
+++ types/variables.ts
@@ -0,0 +1,9 @@
+
+// Not really necessary but just to learn about type annotations
+let FirstName: string = "chinmay"
+
+//can be changed to another string
+FirstName = "not chinmay"
+
+//try changing Firstname to a number
+// FirstName = 9
