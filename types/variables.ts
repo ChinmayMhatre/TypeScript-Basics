c06b1e3a15e95e0c666569b02f45206d4fc9ed28
Chinmay Mhatre <chinmaymhatre12@gmail.com>
2023-02-10 18:37:19 +0530

    basic types

--- types/variables.ts
+++ types/variables.ts
@@ -7,3 +7,23 @@ FirstName = "not chinmay"
 
 //try changing Firstname to a number
 // FirstName = 9
+
+// All types of numbers like float, int are just of the type number(all lowercase)
+
+let RollNo:number = 12
+
+// Change it to different number
+RollNo = 24
+
+// try changing it to a different string
+// RollNo = "25"
+
+// boolean are true or false
+
+let isStudent:boolean = true
+
+// Change value
+isStudent = false
+
+// try changing it to a different type
+// isStudent = 98
