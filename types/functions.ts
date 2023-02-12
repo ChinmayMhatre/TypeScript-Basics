df31e0f9594c03ace4c9ed2b8d1a267d0a26e661
Chinmay Mhatre <chinmaymhatre12@gmail.com>
2023-02-12 14:27:35 +0530

    function types

--- types/functions.ts
+++ types/functions.ts
@@ -15,3 +15,37 @@ const square =  (num:number = 2)=>{
     console.log(num*num);
 }
 
+// return type 
+// specify the type of the return value
+
+const add = (num:number):number =>{
+    return num+num
+}
+    
+// when we use methods like map the parameter types are infered by ts 
+//instead of being of the type "any"
+const numArr = [1,534,23,56,76]
+
+numArr.map((num)=>{
+    return num
+})
+
+// void return type 
+// used when the function should return nothing
+
+const printSomething = ():void =>{
+    console.log("lala");
+}
+//this can be inferred by ts but this is just for learning 
+
+
+// "never" type
+// this is a return type for a function that always throws an exception or never stops executing
+// the loop function in three js can be a good example.
+
+let loop = ():never =>{
+    while(true){
+        console.log("I am stuck");
+        
+    }
+}
