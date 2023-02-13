542bdd3f2b076f29fc56877a556176975e4e5778
Chinmay Mhatre <chinmaymhatre12@gmail.com>
2023-02-13 12:31:05 +0530

    array types

---
+++ types/array.ts
@@ -0,0 +1,20 @@
+export {}
+//basic syntax
+
+// a string array all elements of the array should be strings
+let arr:string[] = ["lala","alala"]
+
+// a number array all elements of the array should be numbers
+let numArr:number[] = [12,22,33]
+
+//alternative syntax
+// string array
+let arr2:Array<string> = ["lala","alala"]
+
+// number array
+let numArr2:Array<number> = [12,22,33]
+
+// multidim array
+let cord:string[][]=[["a","b"],["c","d"]]
+
+// similar can be done for 3d and multi-dimensional arrays
